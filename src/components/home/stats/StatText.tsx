66950080a1e079c3f803a5f8a2d543b687a8cf5b
Muhammad Akmal Hisyam <74256288+akmalhisyammm@users.noreply.github.com>
2022-01-01 16:06:35 +0700

    feat: add country section in home page (#4)
    * refactor(components): use variable to initialize useGlobalStat hook
    
    * feat: add country section in home page

---
+++ src/components/home/stats/StatText.tsx
@@ -0,0 +1,46 @@
+import { Box, Skeleton, Text } from '@chakra-ui/react';
+import CountUp from 'react-countup';
+
+type StatTextProps = {
+  type: 'confirmed' | 'recovered' | 'deaths';
+  data: number;
+  isLoading: boolean;
+};
+
+const StatText = ({ type, data, isLoading }: StatTextProps) => {
+  const textColor = () => {
+    switch (type) {
+      case 'confirmed':
+        return 'orange.400';
+      case 'recovered':
+        return 'teal.400';
+      case 'deaths':
+        return 'red.400';
+    }
+  };
+
+  return (
+    <Skeleton marginY={4} isLoaded={!isLoading} fadeDuration={2}>
+      <Box>
+        <Text
+          color={textColor()}
+          fontSize="1.25em"
+          fontWeight="semibold"
+          textShadow="0px 2px 8px rgba(0, 0, 0, 0.3)">
+          <CountUp start={0} end={data} duration={2} separator="," />
+        </Text>
+        <Text
+          color={textColor()}
+          textTransform="uppercase"
+          textShadow="0px 2px 8px rgba(0, 0, 0, 0.3)"
+          fontWeight="light"
+          fontSize="0.6em"
+          letterSpacing={4}>
+          {type}
+        </Text>
+      </Box>
+    </Skeleton>
+  );
+};
+
+export default StatText;
