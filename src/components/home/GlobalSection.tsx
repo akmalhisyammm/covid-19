a2e0b6ebdd7cca68d98fae18719a5f912fe954b0
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-01-01 15:51:01 +0700

    refactor(components): use variable to initialize useGlobalStat hook

--- src/components/home/GlobalSection.tsx
+++ src/components/home/GlobalSection.tsx
@@ -3,25 +3,39 @@ import { DateTime } from 'luxon';
 import { useGlobalStat } from 'hooks';
 import { SWRGlobalStat } from 'types';
 
-import StatCard from './StatCard';
+import StatCard from './stats/StatCard';
 
 const GlobalSection = () => {
-  const { data, isLoading, isError }: SWRGlobalStat = useGlobalStat();
+  const globalStat: SWRGlobalStat = useGlobalStat();
 
-  if (isError) return <Text>Failed to Fetch Data</Text>;
+  if (globalStat.isError) return <Text>Failed to Fetch Data</Text>;
 
   return (
     <>
       <Box marginBottom={4}>
         <Heading>Global Data</Heading>
-        <Text as="small">Last Updated: {DateTime.fromISO(data?.lastUpdate).toFormat('fff')}</Text>
+        <Text as="small">
+          Last Updated: {DateTime.fromISO(globalStat.data?.lastUpdate).toFormat('fff')}
+        </Text>
       </Box>
 
       <Box>
         <SimpleGrid columns={[1, 1, 3]} gap={4}>
-          <StatCard type="confirmed" data={data?.confirmed.value} isLoading={isLoading} />
-          <StatCard type="recovered" data={data?.recovered.value} isLoading={isLoading} />
-          <StatCard type="deaths" data={data?.deaths.value} isLoading={isLoading} />
+          <StatCard
+            type="confirmed"
+            data={globalStat.data?.confirmed.value}
+            isLoading={globalStat.isLoading}
+          />
+          <StatCard
+            type="recovered"
+            data={globalStat.data?.recovered.value}
+            isLoading={globalStat.isLoading}
+          />
+          <StatCard
+            type="deaths"
+            data={globalStat.data?.deaths.value}
+            isLoading={globalStat.isLoading}
+          />
         </SimpleGrid>
       </Box>
     </>
