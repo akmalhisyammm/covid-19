34e825c4640fc8339cbb854fe0252c394f1b1094
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-01-03 08:22:43 +0700

    fix(layout): add paddingBottom on root element

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -15,6 +15,7 @@ const Layout = ({ title, children }: LayoutProps) => {
   return (
     <Box
       minHeight="100vh"
+      paddingBottom={100}
       transition="0.5s ease-out"
       backgroundColor={colorMode === 'light' ? 'gray.100' : 'gray.900'}>
       <Meta title={title} />
