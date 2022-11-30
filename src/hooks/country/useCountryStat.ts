ca054c49e93bcdcdf69675af04851e852bec3bd8
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-30 09:11:22 +0700

    feat(hooks): add conditional data fetching for swr

--- src/hooks/country/useCountryStat.ts
+++ src/hooks/country/useCountryStat.ts
@@ -1,9 +1,13 @@
 import useSWR from 'swr';
+
 import { GLOBAL_API_URL } from 'constants/api';
 import { fetcher } from 'utils/fetcher';
 
-export const useCountryStat = (countryCode: string) => {
-  const { data, error } = useSWR(`${GLOBAL_API_URL}/countries/${countryCode}`, fetcher);
+export const useCountryStat = (countryCode?: string) => {
+  const { data, error } = useSWR(
+    countryCode ? `${GLOBAL_API_URL}/countries/${countryCode}` : null,
+    fetcher
+  );
 
-  return { data, isLoading: !error && !data, isError: error };
+  return { data, isLoading: !error && !data, isError: !!error };
 };
--- src/hooks/indonesia/useIndonesiaDailyStat.ts
+++ src/hooks/indonesia/useIndonesiaDailyStat.ts
@@ -1,9 +1,10 @@
 import useSWR from 'swr';
+
 import { INDONESIA_API_URL } from 'constants/api';
 import { fetcher } from 'utils/fetcher';
 
 export const useIndonesiaDailyStat = () => {
   const { data, error } = useSWR(`${INDONESIA_API_URL}/harian`, fetcher);
 
-  return { data, isLoading: !error && !data, isError: error };
+  return { data, isLoading: !error && !data, isError: !!error };
 };
