fdbd750b639fead929433a8d296323bc06e5fcb5
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-30 09:43:18 +0700

    chore: setup next-seo and next-sitemap configurations

---
+++ next-seo.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+const defaultSEOConfig = {
+  title: undefined,
+  titleTemplate: '%s | COVID-19',
+  defaultTitle: 'COVID-19',
+  description: 'COVID-19 statistics web app',
+  canonical: 'https://covid19.akmalhisyam.my.id',
+};
+
+module.exports = defaultSEOConfig;
---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://covid19.akmalhisyam.my.id',
+  generateRobotsTxt: true,
+};
